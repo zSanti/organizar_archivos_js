c857ae88216698e03f4b2d4944741f28d1821eca
Santiago Manuel <80847631+zSanti@users.noreply.github.com>
2024-04-18 20:03:03 +0200

    fix: Importing Express in the code

--- organizar.js
+++ organizar.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const express = require('express');
 require('dotenv').config();
 
 // Recoger la ruta del usuario que desea organizar
@@ -11,35 +12,39 @@ const extensiones = [
     ['.exe', '.rar', '.zip', '.jar']
 ];
 
-// Leemos el directorio que el usuario desea organizar
-fs.readdir(rutaBiblioteca, (err, files) => {
-    if (err) {
-        console.error('Error al leer el directorio:', err);
-        return;
-    }
-    // Si encuentra archivos cogeremos este mismo y revisaremos su extension
-    files.forEach(archivo => {
-        // Leemos los archivos con su extension
-        extensiones.forEach((extension, index) => {
-            // SI la extension coincide con alguno de la lista, y termina por dicha extension
-            if (extension.some(ext => archivo.endsWith(ext))) {
-                switch (index) {
-                    case 0:
-                        // Crearemos la carpeta de Documentos por su extensión
-                        mover('Documentos', archivo);
-                        break;
-                    case 1:
-                        // Crearemos la carpeta de Imágenes por su extensión
-                        mover('Imágenes', archivo);
-                        break;
-                    case 2:
-                        // Crearemos la carpeta de Programas por su extensión
-                        mover('Programas', archivo);
-                        break;
-                    default:
-                        console.log('Extensión no reconocida');
+app = express()
+
+app.listen(() => {
+    // Leemos el directorio que el usuario desea organizar
+    fs.readdir(rutaBiblioteca, (err, files) => {
+        if (err) {
+            console.error('Error al leer el directorio:', err);
+            return;
+        }
+        // Si encuentra archivos cogeremos este mismo y revisaremos su extension
+        files.forEach(archivo => {
+            // Leemos los archivos con su extension
+            extensiones.forEach((extension, index) => {
+                // SI la extension coincide con alguno de la lista, y termina por dicha extension
+                if (extension.some(ext => archivo.endsWith(ext))) {
+                    switch (index) {
+                        case 0:
+                            // Crearemos la carpeta de Documentos por su extensión
+                            mover('Documentos', archivo);
+                            break;
+                        case 1:
+                            // Crearemos la carpeta de Imágenes por su extensión
+                            mover('Imágenes', archivo);
+                            break;
+                        case 2:
+                            // Crearemos la carpeta de Programas por su extensión
+                            mover('Programas', archivo);
+                            break;
+                        default:
+                            console.log('Extensión no reconocida');
+                    }
                 }
-            }
+            });
         });
     });
-});
+})
